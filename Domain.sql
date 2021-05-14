495c9f0f45c1309ffd9f96c59be27862bcc80c78
Danko Šimić <83290695+DankoSimic@users.noreply.github.com>
2021-05-14 12:21:29 +0200

    Domain - Update #1
    Dodano zaboravljeno i stavljeni komentari da me sjete gdje sam stao s cime

--- Domain.sql
+++ Domain.sql
@@ -2,6 +2,7 @@ DROP database IF EXISTS domain;
 CREATE database domain;
 use domain;
 
+# Stavljeni svi inicijalni atributi i dodan bloodline_score koji sam zaboravio
 create table regent (
 	id_regent int primary key auto_increment not null,
 	house int not null,
@@ -10,10 +11,12 @@ create table regent (
 	realm varchar (50) not null,
 	bloodline varchar (50),
 	bloodline_strength varchar (50),
+	bloodline_score int,
 	class varchar (50),
 	level int
 );
 
+# Postavljen dio atributa, barem ono što zasad je foreign key
 create table domain (
 	id_domain int primary key auto_increment not null,
 	regent int not null,
@@ -21,4 +24,5 @@ create table domain (
 	army int not null
 );
 
+# Povezan regent s domenom
 alter table domain add foreign key (regent) references regent (id_regent);
