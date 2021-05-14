01a5768ef3573543eae630784f3db3edc0f294c3
Danko Šimić <83290695+DankoSimic@users.noreply.github.com>
2021-05-14 12:44:01 +0200

    Domain - Update #2
    Dodano još nekoliko tablica i njihove veze

--- Domain.sql
+++ Domain.sql
@@ -24,5 +24,35 @@ create table domain (
 	army int not null
 );
 
+# Dodao province zasada samo kako bi stavio veze, poslije dodati jos atributa
+create table province (
+	id_province int primary key auto_increment not null
+);
+
+# Vidi poslije da li dodati fort upkeep tu kada budes uspostavljao turn, income i expenditure
+create table holding (
+	id_holding int primary key auto_increment not null,
+	province int not null,
+	regent int not null,
+	type varchar (50) not null,
+	level int not null,
+	avg_gold_income decimal (3,1),
+	avg_regency_income decimal (3,1),
+	fort boolean
+);
+
+# Za assete kao sto su road, port i ostalo
+create table asset (
+	id_asset int primary key auto_increment not null,
+	province int not null,
+	type_asset varchar (50),
+	upkeep_asset decimal (2,1)
+);
+
 # Povezan regent s domenom
+# Povezao ostalo, jos poslije regenta s provincijom
 alter table domain add foreign key (regent) references regent (id_regent);
+alter table domain add foreign key (province) references province (id_province);
+alter table asset add foreign key (province) references province (id_province);
+alter table holding add foreign key (province) references province (id_province);
+alter table holding add foreign key (regent) references regent (id_regent);
