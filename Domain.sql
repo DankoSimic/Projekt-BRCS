d1183d2ebed37afda572e7760370360f4f3b25b7
Danko Šimić <83290695+DankoSimic@users.noreply.github.com>
2021-06-11 16:24:55 +0200

    Domain - Update #9 (Tables,Insert,Fixes)
    Table modifications for REGENT, LIEUTENANT, PROVINCE, ARMY, FLEET. Fixed foreign keys. Inserted some data.

--- Domain.sql
+++ Domain.sql
@@ -6,19 +6,21 @@ use domain;
 # HOUSE removed as entity and is now an attribute in both regent & lieutenant
 # FLEET added as entity
 
-# TABLE ORDER
-# 1.CHARACTERS / 1.1. REGENT / 1.2 LIEUTENANT
-# 2. / 2.1 DOMAIN / 2.2 PROVINCE / 2.3 ASSET / 2.4 HOLDING
-# 3. / 3.1 TREASURY / 3.2 ITEM
-# 4. / 4.1 ARMY / 4.2 FLEET 
+# ORDER
+# 1.CHARACTERS / "1.1. REGENT" / "1.2 LIEUTENANT"
+# 2. / "2.1 DOMAIN" / "2.2 PROVINCE" / "2.3 ASSET" / "2.4 HOLDING"
+# 3. / "3.1 TREASURY" / "3.2 ITEM"
+# 4. / "4.1 ARMY" / "4.2 FLEET" 
+# 5. / "5.1 FOREIGN KEYS"
 
-# 1.1 REGENT
+# "1.1 REGENT"
 # CURRENTLY DONE
 
 create table regent (
 	id_regent int primary key auto_increment not null,
 	title varchar (50) not null,
 	name varchar (50) not null,
+	race varchar (50) not null,
 	house varchar (50) not null,
 	dynasty varchar (50) not null,
 	realm varchar (50) not null,
@@ -29,7 +31,7 @@ create table regent (
 	level int
 );
 
-# 1.2 LIEUTENANT
+# "1.2 LIEUTENANT"
 # CURRENTLY DONE
 # TYPE is for Lieutenant, Advisor or Hireling
 # REGENT remains not null in case of freelance characters
@@ -40,6 +42,7 @@ create table lieutenant (
 	regent int,
 	name varchar (50) not null,
 	type varchar (50) not null,
+	race varchar (50) not null,
 	house varchar (50),
 	dynasty varchar (50),
 	bloodline varchar (50),
@@ -49,22 +52,21 @@ create table lieutenant (
 	level int
 );
 
-# 2.1 DOMAIN
+# "2.1 DOMAIN"
 # CURRENTLY DONE
 
 create table domain (
 	id_domain int primary key auto_increment not null,
 	name varchar (50) not null,
-	regent int not null,
-	province int not null,
-	army int not null
+	regent int not null
 );
 
-# 2.2 PROVINCE
+# "2.2 PROVINCE"
 # CURRENTLY DONE
 
 create table province (
 	id_province int primary key auto_increment not null,
+	domain int not null,
 	name varchar (50) not null,
 	terrain varchar (50) not null,
 	level int not null,
@@ -73,7 +75,7 @@ create table province (
 	fort boolean
 );
 
-# 2.3 ASSET
+# "2.3 ASSET"
 # CURRENTLY DONE
 # TYPE is for PORT, ROAD, BRIDGE, HOUSE OF WISDOM and similar provincial buildings or non standard assets  
 
@@ -84,7 +86,7 @@ create table asset (
 	upkeep_asset decimal (2,1)
 );
 
-# 2.4 HOLDING
+# "2.4 HOLDING"
 # CURRENTLY DONE
 # TYPE is for LAW, GUILD, TEMPLE, SOURCE
 
@@ -99,7 +101,7 @@ create table holding (
 	fort boolean
 );
 
-# 3.1 TREASURY
+# "3.1 TREASURY"
 # NOT DONE
 
 create table treasury (
@@ -109,7 +111,7 @@ create table treasury (
 	regency int
 );
 
-# 3.2 ITEM
+# "3.2 ITEM"
 # NOT DONE
 
 create table item (
@@ -122,12 +124,13 @@ create table item (
 	consumeable boolean
 );
 
-# 4.1 ARMY
+# "4.1 ARMY"
 # DONE
 # ARMY is now simplified
 
 create table army (
 	id_army int primary key auto_increment not null,
+	domain int,
 	unique_units int,
 	elf_units int,
 	knights int,
@@ -140,11 +143,12 @@ create table army (
 	army_upkeep decimal (5,1)
 );
 
-# 4.2 FLEET
+# "4.2 FLEET"
 # DONE
 
 create table fleet (
 	id_fleet int primary key auto_increment not null,
+	domain int,
 	unique_ships int,
 	hulk int,
 	cog int,
@@ -157,25 +161,41 @@ create table fleet (
 );
 
 
-# Povezan regent s domenom
-# Povezan army s domenom
-# Povezao ostalo, jos poslije regenta s provincijom
-# Povezao house, kao i kod tablica poslije sredi redoslijed house>regent>domena>holding>asset
-# Promjenio veze, sredi redoslijed da bude ti preglednije
+# "5.1 FOREIGN KEYS"
 
 alter table domain add foreign key (regent) references regent (id_regent);
-alter table domain add foreign key (province) references province (id_province);
-alter table domain add foreign key (army) references army (id_army);
+alter table province add foreign key (domain) references domain (id_domain);
 alter table asset add foreign key (province) references province (id_province);
 alter table holding add foreign key (province) references province (id_province);
 alter table holding add foreign key (regent) references regent (id_regent);
+alter table army add foreign key (domain) references domain (id_domain);
+alter table fleet add foreign key (domain) references domain (id_domain);
 alter table treasury add foreign key (domain) references domain (id_domain);
 alter table item add foreign key (treasury) references treasury (id_treasury);
 alter table lieutenant add foreign key (regent) references regent (id_regent);
 
-# Test funkcionalnosti
-insert into regent (house,title,name,realm,bloodline,bloodline_strength,bloodline_score,class,level) values
-(null,'Baron','Amalia','Holy Roman Empire',null,'Minor',24,'Fighter',2)
+# SINGLE DOMAIN insert
+
+insert into regent (title,name,race,house,dynasty,realm,bloodline,bloodline_strength,bloodline_score,class,level) values
+("Baron","Amalia von Erzheim","Human","von Erzheim zu Engen","von Erzheim","Holy Auran Empire","Ku","Minor",24,"Warmistress",4),
+("Baron","Anastasia von Hirschreich","Elf","von Hirschreich","von Hirschreich","Holy Auran Empire","Unknown","Major",40,"Nightblade",6)
+;
+
+insert into lieutenant (regent,name,race,type,class,level) values 
+(1,"Wolfgang von Falkenstein","Human","Lieutenant","Fighter",3),
+(1,"Ernest von Schaffhausen","Human","Lieutenant","Venturer",3),
+(1,"Corrado","Human","Lieutenant","Fighter",3)
+;
+
+insert into domain (name,regent) values
+("Barony of Engen",1),
+("Barony of Stoutgarten",2)
+;
+
+insert into province (domain,name,terrain,level,avg_gold_income,avg_regency_income,fort) values
+(1,"Engen","Hills",8,2,2,1),
+(1,"Hulm","Hills",2,1,1,0),
+(2,"Stoutgarten","Heavy Forest",2,1,1,0)
 ;
 
 insert into item (name,item_type,item_subtype,power_description,consumeable) values
@@ -183,4 +203,6 @@ insert into item (name,item_type,item_subtype,power_description,consumeable) val
 ('Potion of Healing','Potion','','Healing 1d6',1);
 
 select * from regent;
+select * from lieutenant;
+select * from province;
 select * from item;
